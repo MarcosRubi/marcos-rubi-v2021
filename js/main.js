7967afef7f1f0e130f09d9fbc216b6185a753ee9
Daniel <danielhernandez9980@gmail.com>
2021-01-12 19:34:48 -0600

    Seccion certificados responsive

--- js/main.js
+++ js/main.js
@@ -86,6 +86,60 @@ window.onload = () => {
         themeLight.style.display = 'block'
     })
 
+    //CERTIFICATES
+    const images = [
+        { img: 'images/certificado-html.webp' },
+        { img: 'images/certificado-master-css.webp' },
+        { img: 'images/certificado-pug.webp' },
+        { img: 'images/certificado-crear-pagina-web-desde-cero.webp' },
+        { img: 'images/certificado-master-js.webp' }
+    ]
+    let count = 0;
+    const slide = document.querySelector('.slide-show')
+    const overlay = document.querySelector('.certificate__overlay')
+    const galeryImg = document.querySelectorAll('.certificate__content img')
+    const imgSlideShow = document.getElementById('img__slideshow')
+
+    slide.addEventListener('click', (e) => {
+        let back = slide.querySelector('.btn-back')
+        let next = slide.querySelector('.btn-next')
+        let img = slide.querySelector('img')
+        let tgt = e.target;
+
+        if (tgt == back) {
+            if (count > 0) {
+                img.src = images[count - 1].img
+                count--
+            }
+            else {
+                img.src = images[images.length - 1].img
+                count = images.length - 1
+            }
+        }
+        else if (tgt == next) {
+            if (count < images.length - 1) {
+                img.src = images[count + 1].img
+                count++
+            }
+            else {
+                img.src = images[0].img
+                count = 0
+            }
+        }
+    })
+    Array.from(galeryImg).forEach(img => {
+        img.addEventListener('click', (e) => {
+            const imageSelected = +e.target.dataset.certificate
+            imgSlideShow.src = images[imageSelected].img
+            count = imageSelected
+            overlay.style.opacity = 1
+            overlay.style.visibility = "visible"
+        })
+        document.querySelector('.btn-close').addEventListener('click', ()=>{
+            overlay.style.opacity = 0
+            overlay.style.visibility = "hidden"
+        })
+    })
 }
 function isDarkTheme() {
     const route = document.getElementById('theme')
@@ -94,15 +148,15 @@ function isDarkTheme() {
     const themeLight = document.getElementById('themeLight')
 
     theme == 'dark' ? route.href = "css/dark.css" : route.href = ""
-    if(theme == 'dark'){
+    if (theme == 'dark') {
         themeDark.style.display = 'none'
         themeLight.style.display = 'block'
     }
-    else{ 
+    else {
         themeDark.style.display = 'block'
         themeLight.style.display = 'off'
     }
-} 
+}
 //ANIMATIONS
 AOS.init();
 
